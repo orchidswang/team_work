eaaaf86c110b4dba7a49d935c9e06137f4d314cc
ruihanWang <1522662630@163.com>
2022-11-07 20:24:24 +0800

    Signed-off-by: ruihanWang <1522662630@163.com>

--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,15 @@
 
+function display(name) {
+
+    document.getElementById(name).style.display = "block";
+    if(name=='login'&&name=='register'){
+        document.getElementById('mask').style.display = "block"
+    }
+}
+function hide(name) {
+    document.getElementById(name).style.display = "none";
+    if(name=='login'&&name=='register'){
+        document.getElementById('mask').style.display = "none"
+    }
+}
+
