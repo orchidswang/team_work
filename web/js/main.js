5caf75cd885529dcf84c3e0d6cb496d549761ce8
ruihanWang <1522662630@163.com>
2022-11-09 15:08:35 +0800

    Signed-off-by: ruihanWang <1522662630@163.com>

--- web/js/main.js
+++ web/js/main.js
@@ -2,14 +2,27 @@
 function display(name) {
 
     document.getElementById(name).style.display = "block";
-    if(name=='login'&&name=='register'){
-        document.getElementById('mask').style.display = "block"
-    }
+
 }
 function hide(name) {
+
     document.getElementById(name).style.display = "none";
-    if(name=='login'&&name=='register'){
-        document.getElementById('mask').style.display = "none"
-    }
 }
+
+
+
+function addBookmark(id){
+    var ipt = document.querySelector('.inputBookmark')
+    var val = ipt.value;
+    var val2 = ipt.value.concat('/favicon.ico');
+    localStorage.setItem('bookmark'.concat(id),val)
+    localStorage.setItem('icon'.concat(id),val2)
+    document.getElementById('bookmark'.concat(id)).href = localStorage.getItem('bookmark'.concat(id),val);
+    document.getElementById('img'.concat(id)).src = localStorage.getItem('icon'.concat(id),val2);
+    ipt.value = null
+
+}
+
+
+
 
